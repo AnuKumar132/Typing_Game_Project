1539dc216f36e89b27d5f4a139af5ddf7739de93
ANU K <144515263+AnuKumar132@users.noreply.github.com>
2024-11-22 00:04:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+const paragraphs = [
+  "The art of typing is a skill that combines speed and accuracy, making it an essential tool in the digital age. Practicing regularly not only enhances your typing speed but also boosts your productivity in various tasks. With focus and consistency, anyone can master the keyboard and type effortlessly.",
+  "Technology has transformed the way we communicate, work, and learn in the modern world. Mastering typing is crucial to navigating this digital landscape efficiently. As your typing skills improve, you'll find it easier to express ideas and complete tasks swiftly.",
+  "Typing accurately requires concentration and a clear understanding of finger placement on the keyboard. By practicing daily, you can build muscle memory, making your fingers glide effortlessly over the keys. Over time, this skill becomes second nature, saving valuable time.",
+  "Effective typing is not just about speed; it’s also about maintaining accuracy under pressure. A combination of focus and regular practice helps eliminate errors and boosts confidence. With determination, anyone can achieve their typing goals and unlock their full potential.",
+];
+
+const typingText = document.querySelector(".typ p");
+const inpField = document.querySelector(".cls1 .i1");
+const tryAgainBtn = document.querySelector(".con2 button");
+const timeTag = document.querySelector(".time span b");
+const mistakeTag = document.querySelector(".mistake span");
+const wpmTag = document.querySelector(".wpm span");
+const cpmTag = document.querySelector(".cpm span");
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = (mistakes = isTyping = 0);
+
+function loadParagraph() {
+  const ranIndex = Math.floor(Math.random() * paragraphs.length);
+  typingText.innerHTML = "";
+  paragraphs[ranIndex].split("").forEach((char) => {
+    let span = `<span> ${char}</span>`;
+    typingText.innerHTML += span;
+  });
+  typingText.querySelectorAll("span")[0].classList.add("active");
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+  let characters = typingText.querySelectorAll("span");
+  let typedChar = inpField.value.split("")[charIndex];
+  if (charIndex < characters.length - 1 && timeLeft > 0) {
+    if (!isTyping) {
+      timer = setInterval(initTimer, 1000);
+      isTyping = true;
+    }
+    if (typedChar == null) {
+      if (charIndex > 0) {
+        charIndex--;
+        if (characters[charIndex].classList.contains("incorrect")) {
+          mistakes--;
+        }
+        characters[charIndex].classList.remove("correct", "incorrect");
+      }
+    } else {
+      if ((characters[charIndex], innerText == typedChar)) {
+        characters[charIndex].classList.add("correct");
+      } else {
+        mistakes++;
+        characters[charIndex].classList.add("incorrect");
+      }
+      charIndex++;
+    }
+    characters.forEach((span) => span.classList.remove("active"));
+    characters[charIndex].classList.add("active");
+
+    let wpm = Math.round(
+      ((charIndex - mistakes) / 5 / (maxTime - timeLeft)) * 60
+    );
+    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+
+    wpmTag.innerText = wpm;
+    mistakeTag.innerText = mistakes;
+    cpmTag.innerText = charIndex - mistakes;
+  } else {
+    clearInterval(timer);
+    inpField.value = "";
+  }
+}
+
+function initTimer() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+    let wpm = Math.ro;
+    ((charIndex - mistakes) / 5 / (maxTime - timeLeft)) * 60;
+    wpmTag.innerText = wpm;
+  } else {
+    clearInterval(timer);
+  }
+}
+
+function resetGame() {
+  loadParagraph();
+  clearInterval(timer);
+  timeLeft = maxTime;
+  charIndex = mistakes = isTyping = 0;
+  inpField.value = "";
+  timeTag.innerText = timeLeft;
+  wpmTag.innerText = 0;
+  mistakeTag.innerText = 0;
+  cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
